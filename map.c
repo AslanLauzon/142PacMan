0a08a9f056c1feb22c62540e68d92532310c8a7e
LogCalder <158603218+LogCalder@users.noreply.github.com>
2024-04-02 15:54:00 -0400

    Add files via upload

--- map.c
+++ map.c
@@ -12,111 +12,9 @@
 extern char *map, *dot_map;
 extern int width, height;
 
-//int move_actor(int * y, int * x, char direction, int eat_dots) {
-//    return MOVED_OKAY;
-//}
-
-
 int cordIdxCvt (int x,int y){
 
     return (y * 11 + (x - 1));
-}
-
-int is_wall(int y, int x) {
-    int height = 9;
-    int weight = 9;
-    int index = y * 11 + (x - 1);
-
-
-    if (map[index] == 'W') {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-char * load_map(char * filename, int *map_height, int *map_width) {
-    //int dotIndex = 0; //used for testing
-    FILE *fMap = NULL;
-    fMap = fopen(filename, "r");
-    if (fMap == NULL) { //If the map file is not available
-        printf("File not found.\n");
-
-        //closing the file
-        fclose(fMap);
-        return NULL; //Non-zero return to show error has occurred
-    }
-
-    //Creating the holding and map points
-    char * pMap = (char*) malloc((*map_width+2)*(*map_height+2)*sizeof(char));
-    char * pHold = (char*) malloc(sizeof(char*));
-    if (pMap == NULL || pHold == NULL) { //If there is no memory to allocate them to
-        //message to the user
-        printf("Memory is not available");
-        //Closing the file and free the space
-        fclose(fMap);
-        free(pMap);
-        free(pHold);
-        return NULL; //indicating error has occured
-    }
-    //Creating the top and bottom row of the map
-    for (int i = 0; i < *map_width+2; i++){
-        pMap[i]='W';
-        //index++;
-        pMap[(*map_width+1)*(*map_height+2)+i]='W';
-        //printf("%c ", pMap[i]); //test
-    }
-    //printf("\n"); //test
-    //printf("%d!\n", index); //Index testing
-
-    //Getting the real map
-    for (int i = 1; i < (*map_height+1); i++){
-        //Loading the map
-        pMap[(i-1)*(*map_height+2)+*map_height+2]= 'W'; //Creating the left most wall
-        //printf("%d? ", ((i-1)*(*map_height+2)+*map_height+2)); //Index testing
-        //printf("%c ", pMap[i*(*map_height)]); //test
-        for (int j = 1; j < *map_width+1; j++){
-            fscanf(fMap, "%c", &pHold[0]); // Getting the value from the with a holding value
-            if (pHold[0]== '\n'){
-                fscanf(fMap, "%c", &pMap[i*(*map_height+2)+j]); //The next value will be valid and can be taken
-            }
-            else if (pHold[0]==' '){
-                fscanf(fMap, "%c", &pHold[0]); //Spaces will be in twos therefor second must be taken
-                fscanf(fMap, "%c", &pMap[i*(*map_height+2)+j]); //This will be valid
-            }
-            else{
-                pMap[i*(*map_height+2)+j] = pHold[0]; //If the hold passes the condition, pMap can be made that
-            }
-
-            //printf("%c ", pMap[i*(*map_height)+j]); //test
-           // printf("%d* ", (i*(*map_height+2)+j)); //Index test
-
-           //printf("%c ", pMap[i*(*map_height)+j]); //test
-        } //Goes through 9 reps
-        pMap[i*(*map_height+2)+*map_width+1]= 'W'; //Adding the right most wall
-
-        pMap[i*(*map_height+2)+*map_width+1]= 'W'; //Adding the right most wall
-
-    }
-
-    return pMap;
-}
-char * load_dotMap(int *map_height, int *map_width){
-    char * pMap = (char*) malloc((*map_width+2)*(*map_height+2)*sizeof(char*));
-    if (pMap == NULL) { //If there is no memory to allocate them to
-        //message to the user
-        printf("Memory is not available");
-        //Closing the file and free the space
-        free(pMap);
-        return NULL; //indicating error has occured
-    }
-    for (int i = 0; i < (height+2)*(width+2);i++){
-        if (map[i] == 'P' || map[i] == 'G')
-            pMap[i] = ' ';
-        else
-            pMap[i]=map[i];
-    }
-    return pMap;
 }
 
 int move_actor(int * y, int * x, char direction, int eat_dots){
@@ -155,14 +53,12 @@ int move_actor(int * y, int * x, char direction, int eat_dots){
             break;
     }
 
-
     //checks if new position is a wall
     if (is_wall(tempX,tempY)){
-        return MOVED_WALL
+        return MOVED_WALL;
     }
 
-
-    //if it is a valid position (contains no walls) then it moves the actor and updates all the corresponding variables.
+        //if it is a valid position (contains no walls) then it moves the actor and updates all the corresponding variables.
     else {
         if (eat_dots == 1 && dot_map[actorIdx] == '.') {
             dot_map[actorIdx] = ' ';
@@ -185,17 +81,119 @@ int move_actor(int * y, int * x, char direction, int eat_dots){
 }
 
 
+int is_wall(int y, int x) {
+
+    int index = y * 11 + (x - 1);
 
 
+    if (map[index] == 'W') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+char * load_map(char * filename, int *map_height, int *map_width) {
+    int dotIndex = 0; //used for testing
+    FILE *fMap = NULL;
+    fMap = fopen(filename, "r");
+    if (fMap == NULL) { //If the map file is not available
+        printf("File not found.\n");
+
+        //closing the file
+        fclose(fMap);
+        return NULL; //Non-zero return to show error has occurred
+    }
+
+    //Creating the holding and map points
+    char * pMap = (char*) malloc((*map_width)*(*map_height)*sizeof(char*));
+    char * pHold = (char*) malloc(sizeof(char*));
+    if (pMap == NULL || pHold == NULL) { //If there is no memory to allocate them to
+        //message to the user
+        printf("Memory is not available");
+        //Closing the file and free the space
+        fclose(fMap);
+        free(pMap);
+        free(pHold);
+        return NULL; //indicating error has occured
+    }
+    //Creating the top and bottom row of the map
+//    for (int i = 0; i < *map_width+2; i++){
+//        pMap[i]='W';
+//        //index++;
+//        pMap[(*map_width+1)*(*map_height+2)+i]='W';
+//        //printf("%c ", pMap[i]); //test
+//    }
+   //printf("\n"); //test
+   //printf("%d!\n", index); //Index testing
+
+    //Getting the real map
+    for (int i = 0; i < (*map_height); i++){
+        //Loading the map
+//        pMap[(i-1)*(*map_height+2)+*map_height+2]= 'W'; //Creating the left most wall
+        //printf("%d? ", ((i-1)*(*map_height+2)+*map_height+2)); //Index testing
+      //printf("%c ", pMap[i*(*map_height)]); //test
+        for (int j = 0; j < *map_width; j++){
+            fscanf(fMap, "%c", &pHold[0]); // Getting the value from the with a holding value
+            if (pHold[0]== '\n'){
+                fscanf(fMap, "%c", &pMap[i*(*map_height)+j]); //The next value will be valid and can be taken
+            }
+            else if (pHold[0]==' '){
+                fscanf(fMap, "%c", &pHold[0]); //Spaces will be in twos therefor second must be taken
+                fscanf(fMap, "%c", &pMap[i*(*map_height)+j]); //This will be valid
+            }
+            else{
+                pMap[i*(*map_height)+j] = pHold[0]; //If the hold passes the condition, pMap can be made that
+            }
+
+           // printf("%d* ", (i*(*map_height+2)+j)); //Index test
+
+           //printf("%c ", pMap[i*(*map_height)+j]); //test
+        } //Goes through 9 reps
+//        pMap[i*(*map_height+2)+*map_width+1]= 'W'; //Adding the right most wall
+
+    }
+
+    return pMap;
+}
+char * load_dotMap(int *map_height, int *map_width){
+    char * pMap = (char*) malloc((*map_width+2)*(*map_height+2)*sizeof(char*));
+    if (pMap == NULL) { //If there is no memory to allocate them to
+        //message to the user
+        printf("Memory is not available");
+        //Closing the file and free the space
+        free(pMap);
+        return NULL; //indicating error has occured
+    }
+    for (int i = 0; i < (height)*(width);i++){
+        if (map[i] == 'P' || map[i] == 'G')
+            pMap[i] = ' ';
+        else
+            pMap[i]=map[i];
+    }
+    return pMap;
+}
 
 int printMap(int *map_height, int *map_width, int getDots){
     int dots = 0;
+    int level = -1;
     for (int i = 0; i < (*map_height+2)*(*map_width+2); i++){
-        printf("%c ", map[i]);
+        if (i < *map_width+1 || (*map_width+2)*(*map_height+1)< i)
+            printf("W ");
+        else if ((i+1)%11==0){
+            printf("W\n");
+            level++;
+        }
+        else if (i % (*map_width+2)==0)
+            printf("W ");
+        else{
+            printf("%c ", map[i-(width+3)-2*level]);
+        }
+
+
         if (getDots == 1 && map[i] == '.')
             dots++;
-        if ((i+1)%11==0)
-            printf("\n");
+
     }
     return dots;
 }
